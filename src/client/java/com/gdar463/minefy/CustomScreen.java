6ec88f194dcc870dd5abc141d359aa27dec49bfc
gdar463 <dev@gdar463.com>
2025-10-07 22:27:39 +0200

    removed useless stuff
    Signed-off-by: gdar463 <dev@gdar463.com>

--- src/client/java/com/gdar463/minefy/CustomScreen.java
+++ src/client/java/com/gdar463/minefy/CustomScreen.java
@@ -1,34 +1,34 @@
-package com.gdar463.minefy;
-
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.toast.SystemToast;
-import net.minecraft.text.Text;
-import net.minecraft.util.Colors;
-
-public class CustomScreen extends Screen {
-    public CustomScreen(Text title) {
-        super(title);
-    }
-
-    @Override
-    protected void init() {
-        ButtonWidget buttonWidget = ButtonWidget.builder(Text.of("Hello World"), (btn) -> {
-            assert this.client != null;
-            this.client.getToastManager().add(
-                    SystemToast.create(this.client, SystemToast.Type.PERIODIC_NOTIFICATION, Text.of("Hllo"), Text.of("I'm toast"))
-            );
-        }).dimensions(40, 40, 80, 20).build();
-
-        this.addDrawableChild(buttonWidget);
-    }
-
-    @Override
-    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        this.renderBackground(context, mouseX, mouseY, delta);
-        super.render(context, mouseX, mouseY, delta);
-
-        context.drawText(this.textRenderer, "Magic Button", 40, 40 - this.textRenderer.fontHeight - 10, Colors.WHITE, true);
-    }
-}
+//package com.gdar463.minefy;
+//
+//import net.minecraft.client.gui.DrawContext;
+//import net.minecraft.client.gui.screen.Screen;
+//import net.minecraft.client.gui.widget.ButtonWidget;
+//import net.minecraft.client.toast.SystemToast;
+//import net.minecraft.text.Text;
+//import net.minecraft.util.Colors;
+//
+//public class CustomScreen extends Screen {
+//    public CustomScreen(Text title) {
+//        super(title);
+//    }
+//
+//    @Override
+//    protected void init() {
+//        ButtonWidget buttonWidget = ButtonWidget.builder(Text.of("Hello World"), (btn) -> {
+//            assert this.client != null;
+//            this.client.getToastManager().add(
+//                    SystemToast.create(this.client, SystemToast.Type.PERIODIC_NOTIFICATION, Text.of("Hllo"), Text.of("I'm toast"))
+//            );
+//        }).dimensions(40, 40, 80, 20).build();
+//
+//        this.addDrawableChild(buttonWidget);
+//    }
+//
+//    @Override
+//    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+//        this.renderBackground(context, mouseX, mouseY, delta);
+//        super.render(context, mouseX, mouseY, delta);
+//
+//        context.drawText(this.textRenderer, "Magic Button", 40, 40 - this.textRenderer.fontHeight - 10, Colors.WHITE, true);
+//    }
+//}
--- src/client/java/com/gdar463/minefy/MinefyClient.java
+++ src/client/java/com/gdar463/minefy/MinefyClient.java
@@ -1,14 +1,6 @@
 package com.gdar463.minefy;
 
-import com.mojang.brigadier.Command;
-import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,28 +8,20 @@ public class MinefyClient implements ClientModInitializer {
     public static final String MOD_ID = "minefy/client";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public static Screen toOpen;
-    public static int screenTicks = 0;
+//    public static Screen toOpen;
+//    public static int screenTicks = 0;
 
     @Override
     public void onInitializeClient() {
-        ClientTickEvents.START_CLIENT_TICK.register(client -> {
-            if (toOpen != null) {
-                screenTicks++;
-                if (screenTicks > 5) {
-                    client.setScreen(toOpen);
-                    toOpen = null;
-                    screenTicks = 0;
-                }
-            }
-        });
-        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("minefy")
-                .then(ClientCommandManager.literal("debug_screen").executes(this::openDebugScreen))));
-    }
-
-    private int openDebugScreen(CommandContext<FabricClientCommandSource> ctx) {
-        LOGGER.info("Opening debug screen");
-        toOpen = new CustomScreen(Text.of("Debug Screen"));
-        return Command.SINGLE_SUCCESS;
+//        ClientTickEvents.START_CLIENT_TICK.register(client -> {
+//            if (toOpen != null) {
+//                screenTicks++;
+//                if (screenTicks > 5) {
+//                    client.setScreen(toOpen);
+//                    toOpen = null;
+//                    screenTicks = 0;
+//                }
+//            }
+//        });
     }
 }
