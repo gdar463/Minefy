58a648b277a0cb2039712c58e569870905bf316d
gdar463 <dev@gdar463.com>
2025-10-07 22:33:05 +0200

    added license headers to files
    Signed-off-by: gdar463 <dev@gdar463.com>

--- src/main/java/com/gdar463/minefy/Minefy.java
+++ src/main/java/com/gdar463/minefy/Minefy.java
@@ -1,16 +1,32 @@
+/*
+ Copyright (c) gdar463 (Dario) <dev@gdar463.com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.gdar463.minefy;
 
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Minefy implements ModInitializer {
-	public static final String MOD_ID = "minefy";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MOD_ID = "minefy";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		LOGGER.info("Initilizing...");
-	}
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Initilizing...");
+    }
 }
