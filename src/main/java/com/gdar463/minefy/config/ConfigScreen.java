b68f24d37667526cf53e93cc466ccc9441506bcd
gdar463 <dev@gdar463.com>
2025-10-09 00:25:48 +0200

    added spotify tab to config screen
    Signed-off-by: gdar463 <dev@gdar463.com>

--- src/main/java/com/gdar463/minefy/config/ConfigScreen.java
+++ src/main/java/com/gdar463/minefy/config/ConfigScreen.java
@@ -17,11 +17,14 @@
 
 package com.gdar463.minefy.config;
 
+import com.gdar463.minefy.config.controllers.HiddenStringControllerBuilder;
 import dev.isxander.yacl3.api.ConfigCategory;
 import dev.isxander.yacl3.api.Option;
 import dev.isxander.yacl3.api.OptionDescription;
 import dev.isxander.yacl3.api.OptionGroup;
 import dev.isxander.yacl3.api.YetAnotherConfigLib;
+import dev.isxander.yacl3.api.controller.IntegerFieldControllerBuilder;
+import dev.isxander.yacl3.api.controller.StringControllerBuilder;
 import dev.isxander.yacl3.api.controller.TickBoxControllerBuilder;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
@@ -42,7 +45,52 @@ public class ConfigScreen {
                                         .controller(TickBoxControllerBuilder::create).build()
                                 ).build()
                         ).build()
-                ).build()
+                ).category(ConfigCategory.createBuilder()
+                        .name(Text.translatable("text.minefy.config.category.spotify"))
+                        .group(OptionGroup.createBuilder()
+                                .name(Text.translatable("text.minefy.config.spotify.client.name"))
+                                .description(OptionDescription.of(Text.translatable("text.minefy.config.spotify.client.description")))
+                                .collapsed(true)
+                                .option(Option.<String>createBuilder()
+                                        .name(Text.translatable("text.minefy.config.spotify.client.id.name"))
+                                        .description(OptionDescription.of(Text.translatable("text.minefy.config.spotify.client.id.description")))
+                                        .binding(config.spotifyClientId, () -> config.spotifyClientId, val -> config.spotifyClientId = val)
+                                        .controller(HiddenStringControllerBuilder::create)
+                                        .build())
+                                .option(Option.<String>createBuilder()
+                                        .name(Text.translatable("text.minefy.config.spotify.client.redirect.name"))
+                                        .description(OptionDescription.of(Text.translatable("text.minefy.config.spotify.client.redirect.description")))
+                                        .binding(config.spotifyRedirectUri, () -> config.spotifyRedirectUri, val -> config.spotifyRedirectUri = val)
+                                        .controller(StringControllerBuilder::create)
+                                        .build())
+                                .option(Option.<Integer>createBuilder()
+                                        .name(Text.translatable("text.minefy.config.spotify.client.port.name"))
+                                        .description(OptionDescription.of(Text.translatable("text.minefy.config.spotify.client.port.description")))
+                                        .binding(config.spotifyCallbackPort, () -> config.spotifyCallbackPort, val -> config.spotifyCallbackPort = val)
+                                        .controller(opt -> IntegerFieldControllerBuilder.create(opt)
+                                                .range(1024, 65565)
+                                                .formatValue(value -> Text.of(value.toString())))
+                                        .build())
+                                .build())
+                        .group(OptionGroup.createBuilder()
+                                .name(Text.translatable("text.minefy.config.spotify.tokens.name"))
+                                .description(OptionDescription.of(Text.translatable("text.minefy.config.spotify.tokens.description")))
+                                .collapsed(true)
+                                .option(Option.<String>createBuilder()
+                                        .name(Text.translatable("text.minefy.config.spotify.tokens.access.name"))
+                                        .description(OptionDescription.of(Text.translatable("text.minefy.config.spotify.tokens.access.description")))
+                                        .binding(config.spotifyAccessToken, () -> config.spotifyAccessToken, val -> config.spotifyAccessToken = val)
+                                        .controller(HiddenStringControllerBuilder::create)
+                                        .build())
+                                .option(Option.<String>createBuilder()
+                                        .name(Text.translatable("text.minefy.config.spotify.tokens.refresh.name"))
+                                        .description(OptionDescription.of(Text.translatable("text.minefy.config.spotify.tokens.refresh.description")))
+                                        .binding(config.spotifyRefreshToken, () -> config.spotifyRefreshToken, val -> config.spotifyRefreshToken = val)
+                                        .controller(HiddenStringControllerBuilder::create)
+                                        .build())
+                                .build())
+                        .build())
+                .build()
                 .generateScreen(parent);
     }
 }
