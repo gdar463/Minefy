9cebf4698fc7e31acbe67787bba55e37bf1cc85e
gdar463 <dev@gdar463.com>
2026-01-04 22:59:38 +0100

    build: moved stonecutter versions to separate json file
    Signed-off-by: gdar463 <dev@gdar463.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,19 +29,7 @@ stonecutter {
     kotlinController = true
     centralScript = "build.gradle.kts"
 
-    create(rootProject) {
-        /**
-         * @param mcVersion The base minecraft version.
-         * @param loaders A list of loaders to target, supports "fabric" (1.14+), "neoforge"(1.20.6+), "vanilla"(any) or "forge"(<=1.20.1)
-         */
-        fun mc(mcVersion: String, name: String = mcVersion, loaders: Iterable<String>) =
-            loaders.forEach { version("$name-$it", mcVersion) }
-
-        mc("1.21.10", loaders = listOf("fabric", "neoforge"))
-        mc("1.21.1", loaders = listOf("fabric", "neoforge"))
-
-        vcsVersion = "1.21.10-fabric"
-    }
+    create(rootProject, file("versions/versions.json5"))
 }
 
 rootProject.name = "Minefy"
